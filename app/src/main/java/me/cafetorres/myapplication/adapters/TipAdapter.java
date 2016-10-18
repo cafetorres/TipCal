899a2fb8d3a7cb3d94b69cfb0a506d9b59d489b4
Carlos Torres <cf.torresluna@gmail.com>
2016-10-17 22:10:59 -0500

    update tipAdapter.java

--- app/src/main/java/me/cafetorres/myapplication/adapters/TipAdapter.java
+++ app/src/main/java/me/cafetorres/myapplication/adapters/TipAdapter.java
@@ -5,10 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -24,27 +22,21 @@ public class TipAdapter extends RecyclerView.Adapter<TipAdapter.ViewHolder>{
 
     private Context context;
     private List<TipRecord> dataset;
-    private AdapterView.OnItemClickListener onItemClickListener;
+    //private AdapterView.OnItemClickListener onItemClickListener;
 
-    public TipAdapter(Context context, List<TipRecord> dataset, AdapterView.OnItemClickListener onItemClickListener) {
+    public TipAdapter(Context context, List<TipRecord> dataset) {
         this.context = context;
         this.dataset = dataset;
-        this.onItemClickListener = onItemClickListener;
-    }
-
-    public TipAdapter(Context context, AdapterView.OnItemClickListener onItemClickListener) {
-        this.context = context;
-        this.dataset = new ArrayList<TipRecord>();
-        this.onItemClickListener = onItemClickListener;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_row, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
 
         return new ViewHolder(view);
     }
+
+
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
@@ -60,8 +52,13 @@ public class TipAdapter extends RecyclerView.Adapter<TipAdapter.ViewHolder>{
 
     public void  add(TipRecord record){
         dataset.add(0,record);
+        notifyDataSetChanged();
     }
 
+    public void clear() {
+        dataset.clear();
+        notifyDataSetChanged();
+    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
         @Bind(R.id.txtContent)
